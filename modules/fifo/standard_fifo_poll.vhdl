6429e63a92e2b0546f968d26dfbfed58dd65ef8c
John Bieling <john.bieling@gmx.de>
2015-06-16 18:56:12 +0200

    Update standard_fifo_poll.vhdl

--- modules/fifo/standard_fifo_poll.vhdl
+++ modules/fifo/standard_fifo_poll.vhdl
@@ -1,9 +1,27 @@
-----------------------------------------------------------------------------------
---                                                                             
---  Company: University of Bonn                                                
---  Engineer: John Bieling                                                     
---                                                                                                                                                         
-----------------------------------------------------------------------------------
+-------------------------------------------------------------------------
+----                                                                 ----
+---- Company:  University of Bonn                                    ----
+---- Engineer: John Bieling                                          ----
+----                                                                 ----
+-------------------------------------------------------------------------
+----                                                                 ----
+---- Copyright (C) 2015 John Bieling                                 ----
+----                                                                 ----
+---- This program is free software; you can redistribute it and/or   ----
+---- modify it under the terms of the GNU General Public License as  ---- 
+---- published by the Free Software Foundation; either version 3 of  ----
+---- the License, or (at your option) any later version.             ----
+----                                                                 ----
+---- This program is distributed in the hope that it will be useful, ----
+---- but WITHOUT ANY WARRANTY; without even the implied warranty of  ----
+---- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the    ----
+---- GNU General Public License for more details.                    ----
+----                                                                 ----
+---- You should have received a copy of the GNU General Public       ----
+---- License along with this program; if not, see                    ----
+---- <http://www.gnu.org/licenses>.                                  ----
+----                                                                 ----
+-------------------------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
